58ed1169c66a7b95f30b0b7f62af9dfcfb9a0a1a
Yuri Goldfeld <yuri.goldfeld@yahoo.com>
2024-03-25 00:29:31 -0700

    Removed certain minor internal dead code related to macOS.

--- src/flow/async/detail/async.cpp
+++ src/flow/async/detail/async.cpp
@@ -23,9 +23,6 @@
 #ifdef FLOW_OS_LINUX
 #  include <sched.h>
 #endif
-#ifdef FLOW_OS_MAC
-#  include <cpuid.h>
-#endif
 
 namespace flow::async
 {
@@ -39,21 +36,9 @@ uint16_t cpu_idx()
   using ::sched_getcpu;
 
   return ret_int_t(sched_getcpu());
-#elif defined(FLOW_OS_MAC)
-  /* Adapted from: https://stackoverflow.com/questions/33745364/sched-getcpu-equivalent-for-os-x
-   * Their comments omitted, since they wouldn't be to the desired level of quality anyway.  @todo Comment well. */
-  uint32_t cpu_info[4];
-  __cpuid_count(1, 0, cpu_info[0], cpu_info[1], cpu_info[2], cpu_info[3]); // It's a macro.  It loads cpu_info[]s.
-  if ((cpu_info[3] & (1 << 9)) == 0)
-  {
-    return 0;
-  }
-  // else
-  const auto idx = cpu_info[1] >> 24;
-  assert(idx <= numeric_limits<ret_int_t>::max());
-  return ret_int_t(idx);
 #else
-#  error "cpu_idx() implementation is for Linux or Mac only.  Mac way'd likely work in any x86, but that's untested."
+  static_assert(false, "cpu_idx() implementation is for Linux.  "
+                         "Revisit for other OS and/or architectures (x86-64, ARM64, ...).");
 #endif
 }
 
