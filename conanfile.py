b4ded7384aaaf645b656d726af897c5df3fdeb4c
Yuri Goldfeld <yuri.goldfeld@yahoo.com>
2025-01-14 14:31:28 -0800

    (begin work - will test/etc.)

--- conanfile.py
+++ conanfile.py
@@ -109,7 +109,7 @@ class FlowRecipe(ConanFile):
 
     def requirements(self):
         if self.options.build:
-            self.requires("boost/1.84.0")
+            self.requires("boost/1.87.0")
             self.requires("fmt/10.0.0")
 
     def build_requirements(self):
